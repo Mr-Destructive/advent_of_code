62d80914ceec090b6c5406ffebf4b8d0392d7ecb
mr_destructive <gormeet711@gmail.com>
2022-12-01 17:10:02 +0530

    2020: day-1  calorie counting

---
+++ 2022/d1.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"log"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func Log_Error(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func ReadInput(file string) ([]string, error) {
+	content, err := os.ReadFile(file)
+	file_content := string(content)
+	data := strings.Split(file_content, "\n")
+	return data, err
+}
+
+func main() {
+
+	data, err := ReadInput("input1.test")
+	//data, err := ReadInput("input1.txt")
+	Log_Error(err)
+	calories_list := []int{}
+	total_calories := 0
+	max_calory := total_calories
+	for _, d := range data {
+		if d != "" {
+			calories, err := strconv.Atoi(d)
+			Log_Error(err)
+			total_calories += calories
+		} else {
+			calories_list = append(calories_list, total_calories)
+			if total_calories > max_calory {
+				max_calory = total_calories
+			}
+			total_calories = 0
+		}
+	}
+	log.Println("Max Calories: ", max_calory)
+	// Part One 72718
+
+	sum_calories := 0
+	sort.Ints(calories_list)
+	start := len(calories_list) - 3
+	for _, calory := range calories_list[start:] {
+		log.Println(calory)
+		sum_calories += calory
+	}
+	log.Println("Sum of all Calories: ", sum_calories)
+	// Part Two 213089
+}
